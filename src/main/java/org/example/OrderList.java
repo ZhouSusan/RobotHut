c0941dbb5728893eddd8b0fdc4c32bdfd362691f
ZhouSusan <szhou089@gmail.com>
2022-11-15 11:45:37 -0800

    added class OrderList - org.example/OrderList.java

--- src/main/java/org/example/OrderList.java
+++ src/main/java/org/example/OrderList.java
@@ -1,7 +1,14 @@
 package org.example;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderList {
-    private CustomerOrder customerOrder;
+    private CustomerOrder _customerOrder;
+    private List<CustomerOrder> totalOrders = new ArrayList<CustomerOrder>();
+
+    public OrderList(CustomerOrder customerOrder) {
+        this._customerOrder = customerOrder;
+    }
+
+    
 }
