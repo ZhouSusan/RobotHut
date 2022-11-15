3a873d22e47e1498cc076edd7fd4527268770191
ZhouSusan <szhou089@gmail.com>
2022-11-15 09:22:49 -0800

    added constructor and attributes to FoodBase.java

--- src/main/java/Base/FoodBase.java
+++ src/main/java/Base/FoodBase.java
@@ -1,4 +1,19 @@
 package Base;
+import Base.FoodInterface;
 
-public class FoodBase {
+public abstract class FoodBase implements FoodInterface {
+
+    protected int _menuId;
+    protected String _name;
+    protected double _price;
+    protected int _quantity;
+
+    public FoodBase(int menuId, String name, double price, int quantity) {
+        this._menuId = menuId;
+        this._name = name;
+        this._price = price;
+        this._quantity = quantity;
+    }
+
+    
 }
