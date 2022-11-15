1065254282c3e79c570f9357c216805db3fe57a4
ZhouSusan <szhou089@gmail.com>
2022-11-15 10:44:01 -0800

    added Customer Class - org.example/Customer.java

--- src/main/java/org/example/ChefBotApp.java
+++ src/main/java/org/example/ChefBotApp.java
@@ -3,7 +3,9 @@ import Dishes.Burger;
 
 public class ChefBotApp {
     public static void main(String[] args) {
+        Customer customer1 = new Customer("Bob Burgers", 120);
         Burger order1 = new Burger(2);
         System.out.println(order1.getTotal());
+        System.out.println(customer1.checkBalance(customer1.getBalance(), order1.getTotal()));
     }
 }
